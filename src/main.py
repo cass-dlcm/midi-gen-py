9db1a98453cff05c732ca8e57924408e892fcdb4
Cassandra de la Cruz-Munoz <cassandra.delacruzmunoz@gmail.com>
2020-07-05 17:07:49 -0400

    linting on source files

--- src/main.py
+++ src/main.py
@@ -1,14 +1,11 @@
 import random
-from mido import Message, MidiFile, MidiTrack, MetaMessage
+from mido import MidiFile, MidiTrack
 import mido
-import glob
-import os
-import subprocess
 from drum_gen import drum
 
 root = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 cVal = 48
-chords = [   'M',            'm',            '5',            '7',            'M7',           'm7',           'mM7',          '6',            'm6',           'add9',         'madd9',        '7b5',          '7#5',          'm7b5',         'm7#5',]
+chords = [   'M',            'm',            '5',            '7',            'M7',           'm7',           'mM7',          '6',            'm6',           'add9',         'madd9',        '7b5',          '7#5',          'm7b5',         'm7#5']
 chordVals = [[0, 4, 7, 12],  [0, 3, 7, 12],  [0, 7, 12, 19], [0, 4, 7, 10],  [0, 4, 7, 11],  [0, 3, 7, 10],  [0, 3, 7, 11],  [0, 4, 7, 9],   [0, 3, 7, 9],   [0, 2, 4, 7],   [0, 2, 3, 7],   [0, 4, 6, 10],  [0, 4, 8, 10],  [0, 3, 6, 10],  [0, 3, 8, 10]]
 chosenRootsStr = []
 rootVals = []
@@ -31,7 +28,7 @@ for i in range(0, progressionLength):
     outStr = outStr + chosenRootsStr[i] + chosenChordsStr[i] + " "
 
 sequences = []
-for i in range (0, 8):
+for i in range(0, 8):
     temp = []
     tempStrs = []
     for j in range(0, progressionLength):
