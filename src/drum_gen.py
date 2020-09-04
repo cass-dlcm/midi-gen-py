a10964b9df3ecd3c068e6b20def0919e7fdcdbe4
Cassandra de la Cruz-Munoz <cassandra.delacruzmunoz@gmail.com>
2020-09-04 16:09:30 -0400

    finished seperating drum patterns

--- src/drum_gen.py
+++ src/drum_gen.py
@@ -13,117 +13,17 @@ drumTypes = {
     "Acoustic Snare": 0x26,
     "Closed Hi Hat": 0x2a,
     "Pedal Hi Hat": 0x2c,
+    "Open Hi Hat": 0x2e,
     "Crash Cymbal 1": 0x31,
     "Ride Cymbal 1": 0x33,
     "Claves": 0x4b
 }
 
-with open('drum_patterns/Eighth-note ride pattern with triplet swing.json') as json_file:
+with open('drum_patterns/Half time shuffle.json') as json_file:
     data = json.load(json_file)
 
 # Format: (note event, drum type, time (in 1/480 of a quarter note))
 drumPatterns = (
-    # Fill with groove number 2 and crash
-    (("on", "Closed Hi Hat", 0), ("on", "Bass Drum 1", 0), ("off", "Closed Hi Hat", 120), ("on", "Closed Hi Hat", 0),
-        ("off", "Bass Drum 1", 120), ("off", "Closed Hi Hat", 0), ("on", "Closed Hi Hat", 0), ("on", "Acoustic Snare", 0),
-        ("off", "Closed Hi Hat", 120), ("on", "Closed Hi Hat", 0), ("off", "Acoustic Snare", 120), ("off", "Closed Hi Hat", 0),
-        ("on", "Closed Hi Hat", 0), ("on", "Bass Drum 1", 0), ("off", "Closed Hi Hat", 120), ("on", "Closed Hi Hat", 0),
-        ("off", "Bass Drum 1", 120), ("off", "Closed Hi Hat", 0), ("on", "Closed Hi Hat", 0), ("on", "Acoustic Snare", 0),
-        ("off", "Closed Hi Hat", 120), ("off", "Acoustic Snare", 0), ("on", "Acoustic Snare", 0), ("off", "Acoustic Snare", 60),
-        ("on", "Acoustic Snare", 0), ("off", "Acoustic Snare", 60), ("on", "Crash Cymbal 1", 0), ("on", "Bass Drum 1", 0),
-        ("off", "Crash Cymbal 1", 120), ("on", "Closed Hi Hat", 0), ("off", "Bass Drum 1", 120), ("off", "Closed Hi Hat", 0),
-        ("on", "Closed Hi Hat", 0), ("on", "Acoustic Snare", 0), ("off", "Closed Hi Hat", 120), ("on", "Closed Hi Hat", 0),
-        ("off", "Acoustic Snare", 120), ("off", "Closed Hi Hat", 0), ("on", "Closed Hi Hat", 0), ("on", "Bass Drum 1", 0),
-        ("off", "Closed Hi Hat", 120), ("off", "Bass Drum 1", 0), ("on", "Closed Hi Hat", 0), ("on", "Bass Drum 1", 0),
-        ("off", "Closed Hi Hat", 120), ("off", "Bass Drum 1", 0), ("on", "Closed Hi Hat", 0), ("on", "Acoustic Snare", 0),
-        ("off", "Closed Hi Hat", 120), ("on", "Closed Hi Hat", 0), ("off", "Acoustic Snare", 120),
-        ("off", "Closed Hi Hat", 0)),
-
-    # Fill with groove number 2 and sixteenth notes
-    (("on", "Closed Hi Hat", 0), ("on", "Bass Drum 1", 0), ("off", "Closed Hi Hat", 120), ("on", "Closed Hi Hat", 0),
-        ("off", "Bass Drum 1", 120), ("off", "Closed Hi Hat", 0), ("on", "Closed Hi Hat", 0), ("on", "Acoustic Snare", 0),
-        ("off", "Closed Hi Hat", 120), ("on", "Closed Hi Hat", 0), ("off", "Acoustic Snare", 120), ("off", "Closed Hi Hat", 0),
-        ("on", "Closed Hi Hat", 0), ("on", "Bass Drum 1", 0), ("off", "Closed Hi Hat", 120), ("on", "Closed Hi Hat", 0),
-        ("off", "Bass Drum 1", 120), ("off", "Closed Hi Hat", 0), ("on", "Closed Hi Hat", 0), ("on", "Acoustic Snare", 0),
-        ("off", "Acoustic Snare", 120), ("off", "Closed Hi Hat", 0), ("on", "Acoustic Snare", 0), ("off", "Acoustic Snare", 60),
-        ("on", "Acoustic Snare", 0), ("off", "Acoustic Snare", 60), ("on", "Closed Hi Hat", 0), ("on", "Bass Drum 1", 0),
-        ("off", "Closed Hi Hat", 120), ("on", "Closed Hi Hat", 0), ("off", "Bass Drum 1", 120), ("off", "Closed Hi Hat", 0),
-        ("on", "Closed Hi Hat", 0), ("on", "Acoustic Snare", 0), ("off", "Closed Hi Hat", 120), ("on", "Closed Hi Hat", 0),
-        ("off", "Acoustic Snare", 120), ("off", "Closed Hi Hat", 0), ("on", "Closed Hi Hat", 0), ("on", "Bass Drum 1", 0),
-        ("off", "Bass Drum 1", 120), ("off", "Closed Hi Hat", 0), ("on", "Closed Hi Hat", 0), ("on", "Bass Drum 1", 0),
-        ("off", "Bass Drum 1", 120), ("off", "Closed Hi Hat", 0), ("on", "Closed Hi Hat", 0), ("on", "Acoustic Snare", 0),
-        ("off", "Closed Hi Hat", 120), ("on", "Closed Hi Hat", 0), ("off", "Acoustic Snare", 120),
-        ("off", "Closed Hi Hat", 0)),
-
-    # Four to the floor bass drum pattern
-    (("on", "Bass Drum 1", 0), ("off", "Bass Drum 1", 480), ("on", "Bass Drum 1", 0), ("off", "Bass Drum 1", 480),
-        ("on", "Bass Drum 1", 0), ("off", "Bass Drum 1", 480), ("on", "Bass Drum 1", 0),
-        ("off", "Bass Drum 1", 480)),
-
-    # Four to the floor beat
-    (("on", "Closed Hi Hat", 0), ("on", "Bass Drum 1", 0), ("off", "Closed Hi Hat", 240), ("on", "Closed Hi Hat", 0),
-        ("off", "Bass Drum 1", 240), ("off", "Closed Hi Hat", 0), ("on", "Closed Hi Hat", 0), ("on", "Bass Drum 1", 0),
-        ("on", "Acoustic Snare", 0), ("off", "Closed Hi Hat", 240), ("on", "Closed Hi Hat", 0), ("off", "Bass Drum 1", 240),
-        ("off", "Acoustic Snare", 0), ("off", "Closed Hi Hat", 0), ("on", "Closed Hi Hat", 0), ("on", "Bass Drum 1", 0),
-        ("off", "Closed Hi Hat", 240), ("on", "Closed Hi Hat", 0), ("off", "Bass Drum 1", 240), ("off", "Closed Hi Hat", 0),
-        ("on", "Closed Hi Hat", 0), ("on", "Bass Drum 1", 0), ("on", "Acoustic Snare", 0), ("off", "Closed Hi Hat", 240),
-        ("on", "Closed Hi Hat", 0), ("off", "Bass Drum 1", 240), ("off", "Acoustic Snare", 0), ("off", "Closed Hi Hat", 0)),
-
-    # Four-four pattern with open and closed hi-hats
-    (("on", "Closed Hi Hat", 0), ("on", "Bass Drum 1", 0), ("off", "Closed Hi Hat", 120), ("on", "Closed Hi Hat", 0),
-        ("off", "Closed Hi Hat", 120), ("on", 0x2e, 0), ("off", "Bass Drum 1", 240), ("off", 0x2e, 0),
-        ("on", "Closed Hi Hat", 0), ("on", "Acoustic Snare", 0), ("off", "Closed Hi Hat", 120), ("on", "Closed Hi Hat", 0),
-        ("off", "Closed Hi Hat", 120), ("on", 0x2e, 0), ("off", "Bass Drum 1", 240), ("off", 0x2e, 0),
-        ("on", "Closed Hi Hat", 0), ("on", "Bass Drum 1", 0), ("off", "Closed Hi Hat", 120), ("on", "Closed Hi Hat", 0),
-        ("off", "Closed Hi Hat", 120), ("on", 0x2e, 0), ("off", "Bass Drum 1", 240), ("off", 0x2e, 0),
-        ("on", "Closed Hi Hat", 0), ("on", "Acoustic Snare", 0), ("off", "Closed Hi Hat", 120), ("on", "Closed Hi Hat", 0),
-        ("off", "Closed Hi Hat", 120), ("on", 0x2e, 0), ("off", "Bass Drum 1", 240),
-        ("off", 0x2e, 0)),
-
-    # Gallop drum pattern
-    (("on", "Closed Hi Hat", 0), ("on", "Bass Drum 1", 0), ("off", "Bass Drum 1", 240), ("on", "Bass Drum 1", 0),
-        ("off", "Bass Drum 1", 120), ("on", "Bass Drum 1", 0), ("off", "Closed Hi Hat", 120), ("off", "Bass Drum 1", 0),
-        ("on", "Acoustic Snare", 0), ("on", "Closed Hi Hat", 0), ("on", "Bass Drum 1", 0), ("off", "Bass Drum 1", 240),
-        ("on", "Bass Drum 1", 0), ("off", "Bass Drum 1", 120), ("on", "Bass Drum 1", 0), ("off", "Acoustic Snare", 120),
-        ("off", "Closed Hi Hat", 0), ("off", "Bass Drum 1", 0), ("on", "Closed Hi Hat", 0), ("on", "Bass Drum 1", 0),
-        ("off", "Bass Drum 1", 240), ("on", "Bass Drum 1", 0), ("off", "Bass Drum 1", 120), ("on", "Bass Drum 1", 0),
-        ("off", "Closed Hi Hat", 120), ("off", "Bass Drum 1", 0), ("on", "Acoustic Snare", 0), ("on", "Closed Hi Hat", 0),
-        ("on", "Bass Drum 1", 0), ("off", "Bass Drum 1", 240), ("on", "Bass Drum 1", 0), ("off", "Bass Drum 1", 120),
-        ("on", "Bass Drum 1", 0), ("off", "Acoustic Snare", 120), ("off", "Closed Hi Hat", 0), ("off", "Bass Drum 1", 0)),
-
-    # Ghost note drumming
-    (("on", "Closed Hi Hat", 0), ("on", "Bass Drum 1", 0), ("on", "Acoustic Snare", 116), ("off", "Closed Hi Hat", 4),
-        ("on", "Closed Hi Hat", 112), ("on", "Bass Drum 1", 0), ("off", "Bass Drum 1", 8), ("off", "Acoustic Snare", 0),
-        ("on", "Acoustic Snare", 225), ("on", "Closed Hi Hat", 0), ("off", "Closed Hi Hat", 15), ("off", "Bass Drum 1", 0),
-        ("off", "Acoustic Snare", 240), ("off", "Closed Hi Hat", 0), ("on", "Closed Hi Hat", 0), ("on", "Bass Drum 1", 0),
-        ("off", "Closed Hi Hat", 120), ("on", "Acoustic Snare", 0), ("off", "Bass Drum 1", 120), ("off", "Acoustic Snare", 0),
-        ("on", "Closed Hi Hat", 0), ("on", "Bass Drum 1", 0), ("on", "Acoustic Snare", 116), ("off", "Closed Hi Hat", 4),
-        ("on", "Closed Hi Hat", 112), ("on", "Bass Drum 1", 0), ("off", "Bass Drum 1", 8), ("off", "Acoustic Snare", 0),
-        ("on", "Acoustic Snare", 225), ("on", "Closed Hi Hat", 0), ("off", "Closed Hi Hat", 15), ("off", "Bass Drum 1", 0),
-        ("off", "Acoustic Snare", 240), ("off", "Closed Hi Hat", 0), ("on", "Closed Hi Hat", 0), ("on", "Bass Drum 1", 0),
-        ("off", "Closed Hi Hat", 120), ("on", "Acoustic Snare", 0), ("off", "Bass Drum 1", 120),
-        ("off", "Acoustic Snare", 0)),
-
-    # Half time rock pattern
-    (("on", "Closed Hi Hat", 0), ("on", "Bass Drum 1", 0), ("off", "Closed Hi Hat", 240), ("on", "Closed Hi Hat", 0),
-        ("off", "Bass Drum 1", 240), ("off", "Closed Hi Hat", 0),
-        ("on", "Closed Hi Hat", 0), ("on", "Acoustic Snare", 0), ("off", "Closed Hi Hat", 240), ("on", "Closed Hi Hat", 0),
-        ("off", "Acoustic Snare", 240), ("off", "Closed Hi Hat", 0),
-        ("on", "Closed Hi Hat", 0), ("on", "Bass Drum 1", 0), ("off", "Closed Hi Hat", 120), ("off", "Bass Drum 1", 120),
-        ("on", "Closed Hi Hat", 0), ("off", "Closed Hi Hat", 120),
-        ("on", "Closed Hi Hat", 120), ("on", "Acoustic Snare", 0), ("off", "Closed Hi Hat", 120),
-        ("off", "Closed Hi Hat", 120),
-        ("on", "Closed Hi Hat", 0), ("off", "Closed Hi Hat", 120), (0xff01, 120)),
-
-    # Half time shuffle
-    (("on", "Closed Hi Hat", 0), ("on", "Bass Drum 1", 0), ("off", "Closed Hi Hat", 180), ("on", "Closed Hi Hat", 180),
-        ("off", "Bass Drum 1", 180), ("off", "Closed Hi Hat", 0),
-        ("on", "Closed Hi Hat", 0), ("off", "Closed Hi Hat", 180),
-        ("on", "Closed Hi Hat", 180), ("off", "Closed Hi Hat", 180),
-        ("on", "Closed Hi Hat", 0), ("on", "Acoustic Snare", 0), ("off", "Closed Hi Hat", 180), ("on", "Closed Hi Hat", 180),
-        ("off", "Acoustic Snare", 180), ("off", "Closed Hi Hat", 0),
-        ("on", "Closed Hi Hat", 0), ("off", "Closed Hi Hat", 180),
-        ("on", "Closed Hi Hat", 180), ("off", "Closed Hi Hat", 180))
 )
 
 
@@ -146,3 +46,26 @@ def drum(drumTrack):
     pattern = data["pattern"]
     for i in pattern:
         drum_pattern_repeat_recursion(i, drumTrack)
+
+
+def createPatternFile():
+    patternFile = open('drum_patterns.json', 'w')
+    a = []
+    for pattern in drumPatterns:
+        b = {}
+        c = []
+        for note in pattern:
+            print(note)
+            d = {
+                'noteEvent': note[0],
+                'drumType': note[1],
+                'time': note[2]
+            }
+            c.append(d)
+        b['name'] = ''
+        b['pattern'] = c
+        a.append(b)
+    patternFile.write(json.dumps(a, indent=2))
+
+
+# createPatternFile()
